3ec5695f63e4c816ba9c62c80736a705df608b13
松庭灼墨 <1657124562@qq.com>
2026-05-23 17:59:03 +0800

    更新 generate_icons.py

--- generate_icons.py
+++ generate_icons.py
@@ -1,7 +1,7 @@
 import cairosvg
 from PIL import Image
 
-cairosvg.svg2png(url="assets/icon.svg", write_to="icon.png", output_width=512, output_height=512)
-img = Image.open("icon.png")
-img.save("icon.ico", format="ICO", sizes=[(16,16),(32,32),(48,48),(256,256)])
-print("✅ icon.png 和 icon.ico 已生成")
+cairosvg.svg2png(url="assets/icon.svg", write_to="assets/icon.png", output_width=512, output_height=512)
+img = Image.open("assets/icon.png")
+img.save("icon.ico", format="ICO", sizes=[(256,256)])
+print("Done: assets/icon.png and icon.ico generated.")
