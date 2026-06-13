b7ced91935b520b04aa70b6f06fc0fab86f4f799
松庭灼墨 <1657124562@qq.com>
2026-06-13 15:26:15 +0800

    删除 generate_icons.py

--- generate_icons.py
+++
@@ -1,7 +0,0 @@
-import cairosvg
-from PIL import Image
-
-cairosvg.svg2png(url="assets/icon.svg", write_to="assets/icon.png", output_width=512, output_height=512)
-img = Image.open("assets/icon.png")
-img.save("icon.ico", format="ICO", sizes=[(256,256)])
-print("Done: assets/icon.png and icon.ico generated.")
