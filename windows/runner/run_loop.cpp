4729870a2bd174a32ab88332685f2a34cf021a17
松庭灼墨 <1657124562@qq.com>
2026-06-20 17:26:54 +0800

    Delete windows

--- windows/runner/run_loop.cpp
+++
@@ -1,9 +0,0 @@
-#include "run_loop.h"
-RunLoop::RunLoop() {}
-RunLoop::~RunLoop() {}
-void RunLoop::Run() {
-  while (GetMessage(&msg_, nullptr, 0, 0)) {
-    TranslateMessage(&msg_);
-    DispatchMessage(&msg_);
-  }
-}
